5198eb29b58da326f7d6a23d4108cef110136fb7
cristhiandavid96 <davidveleztorres96@gmail.com>
2020-08-10 01:04:51 -0500

    build docs

---
+++ docs/precache-manifest.81a1734475f4f2e203eb75b43a1a3c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631ebb5d08b26ce6ae48649c4c136b0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eb3440e247dd540d616",
+    "url": "/static/css/main.ccf24467.chunk.css"
+  },
+  {
+    "revision": "fe22b5622d1a06633296",
+    "url": "/static/js/2.4bfec5eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4bfec5eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb3440e247dd540d616",
+    "url": "/static/js/main.cbc9ba31.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { CounterApp } from './components/01-useState/CounterApp';
+//import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+//import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { MultipleCustomHooks } from './components/03-example-fetch/MultipleCustomHooks';
+//import HookApp from './HookApp';
+
+
+ReactDOM.render(
+  <MultipleCustomHooks/>,
+  document.getElementById('root')
+);
+
